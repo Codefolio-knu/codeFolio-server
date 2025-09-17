235314f5217d90b2e74b670e24fb3437e7238a19
GyunHee <ef3232@naver.com>
2025-09-17 17:41:40 +0900

    Init: codeFolio server project init

---
+++ src/main/java/com/jandy/codeFolio/CodeFolioApplication.java
@@ -0,0 +1,13 @@
+package com.jandy.codeFolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jandy/codeFolio/CodeFolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jandy.codeFolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
