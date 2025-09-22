2e64215f92816294803404bb12a6bd132491c2c4
GyunHee <ef3232@naver.com>
2025-09-22 10:35:53 +0900

    CF-1 feat: github oauth구현

---
+++ src/main/java/com/jandy/codeFolio/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jandy.codeFolio.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByGithubId(Long id);
+    Optional<User> findByEmail(String email);
+}
