799c7e2aed591277473bb50815528db65b8c1e80
GyunHee <ef3232@naver.com>
2025-09-23 19:34:09 +0900

    CF-1 feat: 전역 에러 로직 추가

---
+++ src/main/java/com/jandy/codeFolio/global/exception/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.jandy.codeFolio.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    // 유저 관련 에러
+    USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    USER_MAIL_INVALID(HttpStatus.BAD_REQUEST, "유효하지 메일 형식입니다. knu.ac.kr 메일을 입력해주세요,"),
+    USER_NOT_ACCESS_FORBIDDEN(HttpStatus.BAD_REQUEST, "접근 권한이 없는 사용자입니다"),
+    USER_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "사용자 저장에 실패했습니다."),
+    USER_MAIL_NOTFOUND(HttpStatus.INTERNAL_SERVER_ERROR, "먼저 메일 인증을 진행해주세요."),
+    USER_CODE_INVALID(HttpStatus.INTERNAL_SERVER_ERROR, "유효하지 않는 인증번호입니다."),
+
+    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
