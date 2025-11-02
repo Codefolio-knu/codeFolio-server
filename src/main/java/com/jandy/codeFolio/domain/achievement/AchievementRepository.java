8648191ef4a0995c96f240f2264b26d9f0fb053a
GyunHee <ef3232@naver.com>
2025-11-02 22:00:26 +0900

    CF-12 feat: 성과 Repository 메서드 추가

--- src/main/java/com/jandy/codeFolio/domain/achievement/AchievementRepository.java
+++ src/main/java/com/jandy/codeFolio/domain/achievement/AchievementRepository.java
@@ -1,6 +1,10 @@
 package com.jandy.codeFolio.domain.achievement;
 
+import com.jandy.codeFolio.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AchievementRepository extends JpaRepository<Achievement, Long> {
+    List<Achievement> findAllByUser(User user);
 }
