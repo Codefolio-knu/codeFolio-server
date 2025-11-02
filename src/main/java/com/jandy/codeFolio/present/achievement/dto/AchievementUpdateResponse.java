0cabb148805785cdc0f28faefcc5bdd69aef9a75
GyunHee <ef3232@naver.com>
2025-11-02 21:27:54 +0900

    CF-12 feat: 성과 수정 dto 추가

---
+++ src/main/java/com/jandy/codeFolio/present/achievement/dto/AchievementUpdateResponse.java
@@ -0,0 +1,10 @@
+package com.jandy.codeFolio.present.achievement.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AchievementUpdateResponse {
+    private Long id;
+}
