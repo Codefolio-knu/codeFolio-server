018f554b6b12742a6651c9ec7164e74f85d86209
GyunHee <ef3232@naver.com>
2025-11-02 21:28:11 +0900

    CF-12 feat: 성과 repository 추가

---
+++ src/main/java/com/jandy/codeFolio/domain/achievement/AchievementRepository.java
@@ -0,0 +1,6 @@
+package com.jandy.codeFolio.domain.achievement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AchievementRepository extends JpaRepository<Achievement, Long> {
+}
