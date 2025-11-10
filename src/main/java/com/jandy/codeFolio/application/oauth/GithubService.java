a76e96c138b7a391aaa0dd7e8dee6b50ed98796b
GyunHee <ef3232@naver.com>
2025-11-10 17:34:54 +0900

    CF-16 feat: github API User Repo조회 및 Repo language조회 로직 구현

--- src/main/java/com/jandy/codeFolio/application/oauth/GithubService.java
+++ src/main/java/com/jandy/codeFolio/application/oauth/GithubService.java
@@ -3,10 +3,12 @@ package com.jandy.codeFolio.application.oauth;
 import com.jandy.codeFolio.domain.user.User;
 import com.jandy.codeFolio.domain.user.UserRepository;
 import com.jandy.codeFolio.global.util.Role;
+import com.jandy.codeFolio.present.oauth.dto.GithubRepoResponse;
 import com.jandy.codeFolio.present.oauth.dto.GithubTokenResponse;
 import com.jandy.codeFolio.present.oauth.dto.GithubUserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -14,7 +16,9 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -28,6 +32,7 @@ public class GithubService {
     @Value("${spring.oauth2.github.client.redirect-uri}") private String redirectUri;
     @Value("${spring.oauth2.github.urls.access-token}") private String tokenUrl;
     @Value("${spring.oauth2.github.urls.user}") private String userUrl;
+    private final String reposUrl = "https://api.github.com/user/repos?per_page=100";
 
     public String getAccessToken(String code) {
         HttpHeaders headers = new HttpHeaders();
@@ -54,6 +59,28 @@ public class GithubService {
 
         ResponseEntity<GithubUserResponse> response = restTemplate.exchange(
                 userUrl, HttpMethod.GET, entity, GithubUserResponse.class);
+
+        return response.getBody();
+    }
+
+    public List<GithubRepoResponse> getRepositories(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<GithubRepoResponse[]> response = restTemplate.exchange(
+                reposUrl, HttpMethod.GET, entity, GithubRepoResponse[].class);
+
+        return Arrays.asList(response.getBody());
+    }
+
+    public Map<String, Long> getLanguages(String languagesUrl, String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Map<String, Long>> response = restTemplate.exchange(
+                languagesUrl, HttpMethod.GET, entity, new ParameterizedTypeReference<>() {});
 
         return response.getBody();
     }
