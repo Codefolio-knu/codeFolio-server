560d2ed26e9ba55f37da4136cb23d62450ff59b3
GyunHee <ef3232@naver.com>
2025-11-11 21:03:19 +0900

    CF-16 docs: Github Project 리스트 조회 문서화

---
+++ src/main/java/com/jandy/codeFolio/present/project/ProjectControllerDocs.java
@@ -0,0 +1,21 @@
+package com.jandy.codeFolio.present.project;
+
+import com.jandy.codeFolio.present.project.dto.ProjectResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Tag(name = "Project API", description = "프로젝트 API 명세")
+public interface ProjectControllerDocs {
+
+    @Operation(summary = "프로젝트 목록 조회", description = "사용자의 프로젝트 목록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "404", description = "USER_NOT_FOUND (사용자 ID가 존재하지 않음)")
+    })
+    List<ProjectResponse> findAllProjects(@PathVariable Long userId);
+}
