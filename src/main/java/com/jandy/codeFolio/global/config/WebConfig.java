5175bd842279c028415d33f0a53bcdbc284865ad
GyunHee <ef3232@naver.com>
2025-11-12 11:36:07 +0900

    feat: CORS 설정 추가

---
+++ src/main/java/com/jandy/codeFolio/global/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.jandy.codeFolio.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${front.base-url}")
+    private String frontBaseUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontBaseUrl)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
