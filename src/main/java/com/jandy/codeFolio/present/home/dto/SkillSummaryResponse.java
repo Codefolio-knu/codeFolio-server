7510f6573d123251d3667a4d8ec3c5600f5bc0fe
GyunHee <ef3232@naver.com>
2025-11-17 14:38:35 +0900

    CF-15 feat: home 조회 DTO 추가

---
+++ src/main/java/com/jandy/codeFolio/present/home/dto/SkillSummaryResponse.java
@@ -0,0 +1,12 @@
+package com.jandy.codeFolio.present.home.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SkillSummaryResponse {
+    private String name;
+}
---
+++ src/main/java/com/jandy/codeFolio/present/home/dto/UserSummaryResponse.java
@@ -0,0 +1,18 @@
+package com.jandy.codeFolio.present.home.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSummaryResponse {
+    private String name;
+    private String bio;
+    private String major;
+    private List<SkillSummaryResponse> skills;
+    private List<AchievementSummaryResponse> achievements;
+}
