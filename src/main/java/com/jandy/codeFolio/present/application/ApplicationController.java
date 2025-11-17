e68bca5125a43741f03b0440547b4b325659e079
GyunHee <ef3232@naver.com>
2025-11-17 16:10:07 +0900

    CF-19 feat: 지원하기 수정 및 삭제 컨트롤러 구현

--- src/main/java/com/jandy/codeFolio/present/application/ApplicationController.java
+++ src/main/java/com/jandy/codeFolio/present/application/ApplicationController.java
@@ -5,6 +5,7 @@ import com.jandy.codeFolio.global.util.ApiResponseWrapper;
 import com.jandy.codeFolio.present.application.dto.ApplicationCreateRequest;
 import com.jandy.codeFolio.present.application.dto.ApplicationCreateResponse;
 import com.jandy.codeFolio.present.application.dto.ApplicationStatusUpdateRequest;
+import com.jandy.codeFolio.present.application.dto.ApplicationUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +31,22 @@ public class ApplicationController implements ApplicationControllerDocs {
             @RequestBody ApplicationStatusUpdateRequest request,
             @RequestParam Long userId) {
         applicationService.updateApplicationStatus(applicationId, request, userId);
+        return ResponseEntity.ok(ApiResponseWrapper.success(HttpStatus.OK, null));
+    }
+
+    @PatchMapping("/{applicationId}/content")
+    public ResponseEntity<ApiResponseWrapper<Void>> updateApplicationContent(
+            @PathVariable Long applicationId,
+            @RequestBody ApplicationUpdateRequest request) {
+        applicationService.updateApplicationContent(applicationId, request);
+        return ResponseEntity.ok(ApiResponseWrapper.success(HttpStatus.OK, null));
+    }
+
+    @DeleteMapping("/{applicationId}")
+    public ResponseEntity<ApiResponseWrapper<Void>> deleteApplication(
+            @PathVariable Long applicationId,
+            @RequestParam Long userId) {
+        applicationService.deleteApplication(applicationId, userId);
         return ResponseEntity.ok(ApiResponseWrapper.success(HttpStatus.OK, null));
     }
 }
