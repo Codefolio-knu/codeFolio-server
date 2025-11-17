9532875a0742c4c00e89393aa345e66106dd5fe7
GyunHee <ef3232@naver.com>
2025-11-17 15:54:49 +0900

    CF-19 feat: 지원하기 수락/거절 로직 구현

--- src/main/java/com/jandy/codeFolio/present/application/ApplicationController.java
+++ src/main/java/com/jandy/codeFolio/present/application/ApplicationController.java
@@ -4,6 +4,7 @@ import com.jandy.codeFolio.application.application.ApplicationService;
 import com.jandy.codeFolio.global.util.ApiResponseWrapper;
 import com.jandy.codeFolio.present.application.dto.ApplicationCreateRequest;
 import com.jandy.codeFolio.present.application.dto.ApplicationCreateResponse;
+import com.jandy.codeFolio.present.application.dto.ApplicationStatusUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,16 +12,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/posts/{postId}/applications")
+@RequestMapping("/api/applications")
 public class ApplicationController implements ApplicationControllerDocs {
 
     private final ApplicationService applicationService;
 
     @PostMapping
     public ResponseEntity<ApiResponseWrapper<ApplicationCreateResponse>> createApplication(
-            @PathVariable Long postId,
             @RequestBody ApplicationCreateRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponseWrapper.success(HttpStatus.CREATED, applicationService.createApplication(postId, request)));
+                .body(ApiResponseWrapper.success(HttpStatus.CREATED, applicationService.createApplication(request.getPostId(), request)));
+    }
+
+    @PatchMapping("/{applicationId}")
+    public ResponseEntity<ApiResponseWrapper<Void>> updateApplicationStatus(
+            @PathVariable Long applicationId,
+            @RequestBody ApplicationStatusUpdateRequest request,
+            @RequestParam Long userId) {
+        applicationService.updateApplicationStatus(applicationId, request, userId);
+        return ResponseEntity.ok(ApiResponseWrapper.success(HttpStatus.OK, null));
     }
 }
