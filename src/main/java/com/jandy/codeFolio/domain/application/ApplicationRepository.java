658313dead3c1e3cae4c7ed30a7e5032c3b5ce7b
GyunHee <ef3232@naver.com>
2025-11-17 15:54:15 +0900

    CF-19 feat: 지원하기 Repository 수정

--- src/main/java/com/jandy/codeFolio/domain/application/ApplicationRepository.java
+++ src/main/java/com/jandy/codeFolio/domain/application/ApplicationRepository.java
@@ -4,7 +4,10 @@ import com.jandy.codeFolio.domain.post.Post;
 import com.jandy.codeFolio.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
     boolean existsByPostAndUser(Post post, User user);
     long countByPostAndStatus(Post post, ApplicationStatus status);
+    List<Application> findAllByPost(Post post);
 }
