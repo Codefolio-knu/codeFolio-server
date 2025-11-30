7da17be780a31070ba4f9d7dd63ba326d124e5ef
GyunHee <ef3232@naver.com>
2025-11-30 13:54:39 +0900

    feat: 교수 로그인 로직 구현

---
+++ src/main/java/com/jandy/codeFolio/application/professor/ProfessorService.java
@@ -0,0 +1,51 @@
+package com.jandy.codeFolio.application.professor;
+
+
+import com.jandy.codeFolio.domain.user.User;
+import com.jandy.codeFolio.domain.user.UserRepository;
+import com.jandy.codeFolio.global.exception.CodeFolioRuntimeException;
+import com.jandy.codeFolio.global.exception.ErrorCode;
+import com.jandy.codeFolio.global.util.Role;
+import com.jandy.codeFolio.present.professor.dto.ProfessorLoginRequest;
+import com.jandy.codeFolio.present.professor.dto.ProfessorSignupRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProfessorService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public User signup(ProfessorSignupRequest request) {
+        User professor = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .bio(request.getBio())
+                .isPublic(request.getIsPublic())
+                .role(Role.PROFESSOR)
+                .emailVerified(true)
+                .build();
+        return userRepository.save(professor);
+    }
+
+    @Transactional(readOnly = true)
+    public User login(ProfessorLoginRequest request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new CodeFolioRuntimeException(ErrorCode.LOGIN_FAILED));
+
+        if (user.getRole() != Role.PROFESSOR) {
+            throw new CodeFolioRuntimeException(ErrorCode.LOGIN_FAILED);
+        }
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new CodeFolioRuntimeException(ErrorCode.LOGIN_FAILED);
+        }
+        return user;
+    }
+}
